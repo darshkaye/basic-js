42c92f754405b519aa834ad6a709c290da87fbe8
Darya Yermakova <yermakova.dasha@gmail.com>
2020-10-06 01:57:54 +0300

    feat: what season realisation

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date == undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  if (isNaN(date.getTime())) {
+    throw new CustomError('Unable to determine the time of year!');
+  }
+  let season = '';
+  switch(date.getMonth() + 1) {
+    case 12:
+    case 1:
+    case 2:
+        season = 'winter';
+    break;
+    case 3:
+    case 4:
+    case 5:
+        season = 'spring';
+    break;
+    case 6:
+    case 7:
+    case 8:
+        season = 'summer';
+    break;
+    case 9:
+    case 10: 
+    case 11:
+        season = 'fall';
+    break;
+  }
+  return season;
 };
