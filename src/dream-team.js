2cb25bc28fc8a1091289dc2677248bb7bacd5a22
Darya Yermakova <yermakova.dasha@gmail.com>
2020-10-05 23:22:50 +0300

    feat: dream-team function realisation

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,8 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members == undefined || !Array.isArray(members) || members.length == 0) {
+    return false;
+  }
+  
+  members = members.map(member => (typeof member == 'string') ? member.trim().charAt(0).toUpperCase() : '');
+  return members.sort().join('');
 };
