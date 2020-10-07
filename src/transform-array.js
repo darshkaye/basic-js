fa2be4a4df798bb5078e7077f98212f997c5f0a1
Darya Yermakova <yermakova.dasha@gmail.com>
2020-10-07 21:48:09 +0300

    feat: transform array realisation

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (arr === undefined || !(arr instanceof Array)) {
+    throw new Error('Works only with arrays');
+  }
+  const discardNext = '--discard-next',
+        discardPrev = '--discard-prev',
+        doubleNext = '--double-next',
+        doublePrev = '--double-prev';
+  let transFormedArr = [...arr];
+  arr.reduce(function(transFormedArr, el, index) {
+    switch (el) {
+      case discardNext:
+        if (transFormedArr[index + 1] !== undefined) {
+          transFormedArr[index + 1] = undefined;
+        }
+        transFormedArr[index] = undefined;
+        break;
+      case discardPrev:
+        if (transFormedArr[index - 1] !== undefined) {
+          transFormedArr[index - 1] = undefined;
+        }
+        transFormedArr[index] = undefined;
+        break;
+      case doubleNext:
+        if (transFormedArr[index + 1] !== undefined) {
+          transFormedArr[index] = transFormedArr[index + 1];
+        } else {
+          transFormedArr[index] = undefined;
+        }
+        break;
+      case doublePrev:
+        if (transFormedArr[index - 1] !== undefined) {
+          transFormedArr[index] = transFormedArr[index - 1];
+        } else {
+          transFormedArr[index] = undefined;
+        }
+        break;
+      default:
+        break;
+    }
+    return transFormedArr;
+  }, transFormedArr)
+  return transFormedArr.filter(el => el !== undefined);
 };
