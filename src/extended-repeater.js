09180e3be579f24dac9f7526501b2f3032683c65
Darya Yermakova <yermakova.dasha@gmail.com>
2020-10-08 00:39:19 +0300

    feat: extended repeater realisation

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,42 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  let result = [], addition = [];
+  const defaultSeparator = '+',
+        defaultAdditionSeparator = '|';
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (options.addition === undefined) {
+    options.addition = '';
+    options.additionSeparator = '';
+  }
+  if (options.additionRepeatTimes === undefined || !Number.isInteger(options.additionRepeatTimes)) {
+    options.additionSeparator = '';
+    options.additionRepeatTimes = 1;
+  }
+
+  if (
+    options.repeatTimes === undefined || 
+    !Number.isInteger(options.repeatTimes)
+  ) {
+    options.repeatTimes = 1;
+  }
+
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = defaultAdditionSeparator;
+  }
+  options.addition = options.addition !== null ? options.addition.toString() : 'null';
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    addition.push(options.addition); 
+  }
+  addition = addition.join(options.additionSeparator);
+
+  if(options.separator === undefined) {
+    options.separator = defaultSeparator;
+  }
+  str = str !== null ? str.toString() : 'null';
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result.push(str + addition); 
+  }
+  result = result.join(options.separator);
+
+  return result;
 };
   
