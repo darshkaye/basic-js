cd55b794ac04aee0f5099717bac081a1fae14988
Darya Yermakova <yermakova.dasha@gmail.com>
2020-10-09 02:12:19 +0300

    feat: VigenereCipher realisation

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,77 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirectDirection = true) {
+    this.isDirectDirection = isDirectDirection ? true : false;
+    this.alphabeth = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    this.cryptoTable = [];
+    for (let i = 0; i < this.alphabeth.length; i++) {
+      this.cryptoTable[i] = this.alphabeth.slice(i).concat(this.alphabeth.slice(0, i));
+    }
+  }
+  encrypt(string, key) {
+    this.validateInputs(string, key);
+
+    let result = "";
+
+    string = string.toUpperCase();
+    key = this.generateKey(string, key);
+
+    for (var i = 0; i < string.length; i++) {
+      let col = this.alphabeth.indexOf(string[i]),
+          row = this.alphabeth.indexOf(key[i]);
+      if (col !== -1) {
+        result += this.cryptoTable[col][row];
+      } else {
+        result += string[i];
+      }
+    }
+    
+    return this.isDirectDirection ? result : result.split("").reverse().join("");
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(string, key) {
+    this.validateInputs(string, key);
+
+    let result = "";
+    string = string.toUpperCase();
+    key = this.generateKey(string, key);
+
+    for (var i = 0; i < string.length; i++) {
+        let row = this.alphabeth.indexOf(key[i]);
+        if (row == -1) {
+          result += string[i];
+          continue;
+        }
+        let col = this.cryptoTable[row].indexOf(string[i]);
+        if (col !== -1) {
+          result += this.alphabeth[col];
+        } else {
+          result += string[i];
+        }
+    }
+
+    return this.isDirectDirection ? result : result.split("").reverse().join("");
+  }
+  validateInputs(string, key) {
+    if (string == undefined || key == undefined) {
+      throw new Error('All input parameters are requiered');
+    }
+  }
+  generateKey(string, key) {
+    let newKey = "",
+        currentKeyPosition = 0;
+
+    for (let i = 0; i < string.length; i++) {
+      if (this.alphabeth.indexOf(string[i]) !== -1) {
+        if (currentKeyPosition >= key.length) {
+          currentKeyPosition = 0
+        }
+        newKey += key[currentKeyPosition];
+        currentKeyPosition++;
+      } else {
+        newKey += string[i];
+      }
+    }
+
+    return newKey.toUpperCase();
   }
 }
 
